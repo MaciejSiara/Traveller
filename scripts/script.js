13958604df244bcdb19ea86a481fdfe6162101a8
Maciej Siara <siaramaciek1@gmail.com>
2020-05-01 22:27:42 +0200

    toTop button has been added

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,76 @@ window.onscroll = () => {
 
   onloadScrollPosition = currentScrollPosition;
 };
+
+//toTop btn
+(function () {
+  let curHeight;
+  const toTopBtn = document.querySelector(".toTop");
+
+  window.addEventListener("scroll", () => {
+    curHeight = window.innerHeight;
+    if (window.pageYOffset > curHeight * 0.5) {
+      toTopBtn.classList.remove("jumpDown");
+      toTopBtn.classList.add("jumpTop");
+    } else {
+      toTopBtn.classList.remove("jumpTop");
+      toTopBtn.classList.add("jumpDown");
+    }
+  });
+
+  toTopBtn.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+})();
+
+// appear stuff
+(function () {
+  let items = [
+    {
+      item: document.querySelector(".locations__head-title"),
+      position: 0,
+    },
+    {
+      item: document.querySelector(".locations__head-info"),
+      position: 0,
+    },
+    {
+      item: document.querySelector(".benefits__head__title"),
+      position: 0,
+    },
+    {
+      item: document.querySelector(".benefits__head__info"),
+      position: 0,
+    },
+    {
+      item: document.querySelector(".cards"),
+      position: 0,
+    },
+    {
+      item: document.querySelector(".form"),
+      position: 0,
+    },
+  ];
+
+  window.addEventListener("scroll", () => {
+    items[0].position = items[0].item.getBoundingClientRect().top;
+    let screenPosition = window.pageYOffset / 1.3;
+    if (items[0].position < screenPosition) {
+      items[0].item.classList.add("appear-content");
+    }
+
+    items.forEach((el) => {
+      el.position = el.item.getBoundingClientRect().top;
+      let screenPosition = window.innerHeight / 1.4;
+
+      if (el.position < screenPosition) el.item.classList.add("appear-content");
+      screenPosition += window.innerHeight / 1.4;
+    });
+    console.log(items);
+    console.log(screenPosition);
+  });
+})();
